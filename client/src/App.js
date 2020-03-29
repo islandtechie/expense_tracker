38244ea8a1271896dfc5c7fd066354fa871f8cf6
Berlin Smith <berlin.smithjr@gmail.com>
2020-03-29 14:06:57 -0400

    working on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import GlobalState from './context/GlobalState';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <GlobalState>
+      <div className="App">
+        <Router>
+          <header className="main-header">
+              <p>Hello</p>
+          </header>
+        </Router>
+      </div>
+    </GlobalState>
   );
 }
 
--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -2,35 +2,21 @@ import React, { useReducer } from 'react';
 import GlobalContext from './globalContext';
 import GlobalReducer from './globalReducer';
 
-const GlobalState = (props) => {
-    const initState = {
-        isAuthenticated: false,
-        sessionID: null,
-        user: {},
-        error: {
-            isError: false,
-            message: ''
-        },
-        expenses: []
+const GlobalState = props => {
+    const initialState = {
+       
     }
 }
 
-const [state, dispatch] = useReducer(GlobalReducer, initState);
+const [state, dispatch] = useReducer(GlobalReducer, initialState);
 
 return (
     <GlobalContext.Provider
-    value={{
-        isAuthenticated: state.isAuthenticated,
-        authenticate,
-        error: state.error,
-        logout,
-        addExpense,
-        expenses: state.expenses,
-        user: state.user,
-        editExpense,
-        deleteUserExpense
-    }}
+        value={{
+        }}
     >
         {props.children}
     </GlobalContext.Provider>
 )
+
+export default GlobalState;
