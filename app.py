22512b9dbbad88fae8f1d10477496ab2a77cab79
Berlin Smith <berlin.smithjr@gmail.com>
2020-03-29 12:54:37 -0400

    python now serving react app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'testinf new flask install'
+    return render_template('index.html')
